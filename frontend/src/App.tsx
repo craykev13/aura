2ff4ae3d918ca7176698f6cc9a654b5605ce1d0a
craykev13 <crumpetkevin@gmail.com>
2026-06-28 03:06:55 -0500

    first load screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,11 @@
 import AmbientBackground from "./components/ambient/AmbientBackground";
+import LoadingScreen from "./pages/LoadingScreen";
 
 export default function App() {
   return (
     <>
       <AmbientBackground />
-
-      <main
-        style={{
-          position: "relative",
-          zIndex: 1,
-          color: "white",
-          padding: "40px",
-        }}
-      >
-        <h1>AURA</h1>
-      </main>
+      <LoadingScreen />
     </>
   );
 }
